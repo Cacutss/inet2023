8c4711a0cf0fe8fdb2de67d3ecface4ccf8cd56d
Cacutss <cuentasmurfdeleague@gmail.com>
2023-11-23 18:44:49 -0300

    comiteado papu

--- php/conseguirNoticia.php
+++ php/conseguirNoticia.php
@@ -1,6 +1,6 @@
 <?php
     function conseguirNoticia($conexion){
-        $sql="SELECT * FROM noticia ORDER BY date_register ASC LIMIT 7";
+        $sql="SELECT * FROM noticia ORDER BY date_register DESC LIMIT 7";
         $result=mysqli_query($conexion,$sql);
         $cont=0;
         while($noticia=mysqli_fetch_assoc($result)){
